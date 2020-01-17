2d618a8f6edbe552fa520878c39c0af90ab06e43
noukta <no9ta-ahmed@live.fr>
2020-01-17 17:59:58 +0100

    fixing leaderboard

--- Assets/Script/GUI/MenuScreen.cs
+++ Assets/Script/GUI/MenuScreen.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using GooglePlayGames;
 using GooglePlayGames.BasicApi;
+using UnityEngine.UI;
 
 public class MenuScreen : MonoBehaviour
 {
     public LevelManager levelManager;
     public Animator levelScreen;
+    public Image title;
 
     private readonly string leaderboardId = GPGSIds.leaderboard_best_score;
 
     public void Start()
     {
         PlayGamesPlatform.Activate();
-        Debug.Log("Activated");
         if (!Social.localUser.authenticated)
         {
             Social.localUser.Authenticate((bool success) => {});
@@ -38,24 +39,12 @@ public class MenuScreen : MonoBehaviour
         if (Social.localUser.authenticated)
         {
             //Update Score
-            PlayGamesPlatform.Instance.LoadScores(leaderboardId, LeaderboardStart.PlayerCentered, 1, LeaderboardCollection.Public, LeaderboardTimeSpan.AllTime,
-            (LeaderboardScoreData data) =>
-                {
-                    if(data.PlayerScore.value < DataManager.instance.Highscore)
-                        Social.ReportScore(DataManager.instance.Highscore, leaderboardId,
-                        (bool success) =>
-                            {
-                                // show leaderboard UI
-                                if(success)
-                                    //PlayGamesPlatform.Instance.ShowLeaderboardUI(leaderboardId);
-                                    Social.ShowLeaderboardUI();
-                            });
-                    else
-                        // show leaderboard UI
-                        //PlayGamesPlatform.Instance.ShowLeaderboardUI(leaderboardId);
-                        Social.ShowLeaderboardUI();
-                }
-            );
+            Social.ReportScore(DataManager.instance.Highscore, leaderboardId,(bool success) =>
+            {
+                // show leaderboard UI
+                if(success)
+                    PlayGamesPlatform.Instance.ShowLeaderboardUI(leaderboardId);
+            });
         }
         else
         {
